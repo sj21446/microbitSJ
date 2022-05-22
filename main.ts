0cecabe6976a6b33761918295f0703a5250e55b5
sj21446 <66420457+sj21446@users.noreply.github.com>
2022-05-22 19:11:05 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Elisabeth")
+    basic.showIcon(IconNames.Chessboard)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Sarah and Elisabeth")
+    basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    basic.clearScreen()
+})
+basic.forever(function () {
+	
+})
